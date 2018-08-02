340ff377fb7489a157084b3fdc0c16eef1ecfc45
Wagner D'Amaral <32019817+wdamaral@users.noreply.github.com>
2018-08-02 15:01:50 -0400

    Create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,18 @@
+const expect = require('expect');
+
+const {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'Wagner';
+        var text = 'Testing case';
+        
+        var message = generateMessage(from, text)
+        
+        expect(typeof message.createdAt).toBe('number');
+        expect(message).toMatchObject({
+            from,
+            text
+        });
+    });
+})
